4e9fca49dfcdb2b45e2b00e8b671fa484493610d
hayper1919 <hayper1919@gmail.com>
2020-12-20 17:06:03 +0700

    Stuff

--- update-modules.js
+++ update-modules.js
@@ -8,8 +8,8 @@ axios.get('https://replicate.npmjs.com/_all_docs')
 	const packageList = response.data;
 	packageList.rows = packageList.rows.sort();
 	console.log(`[!] Found ${packageList.total_rows} package [!]`);
-	eval(`packagejson.dependencies = {\n${packageList.rows.filter(packageData => packageData.id !== "all-module").map(packageData => "\"" +packageData.id + "\": 'latest'").join(',\n')}\n}`);
-	index = `module.exports = {\n${packageList.rows.filter(packageData => packageData.id !== "all-module").map(packageData => "\"" +packageData.id + "\": require('" + packageData.id + "')").join(',\n')}\n}`;
+	eval(`packagejson.dependencies = {\n${packageList.rows.filter(packageData => packageData.id !== "@xhayper/all-module").map(packageData => "\"" +packageData.id + "\": 'latest'").join(',\n')}\n}`);
+	index = `module.exports = {\n${packageList.rows.filter(packageData => packageData.id !== "@xhayper/all-module").map(packageData => "\"" +packageData.id + "\": require('" + packageData.id + "')").join(',\n')}\n}`;
 	console.log("[!] Writing package list to package.json [!]");
 	fs.writeFile('./package.json', JSON.stringify(packagejson, null, 2), () => {});
 	console.log("[!] Writing export script to index.js [!]");
